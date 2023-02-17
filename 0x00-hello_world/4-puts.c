7146390f0bb00e61802c05b6b48e35298f4a43c9
Buraimoh Solomon <Sbolayiwola@gmail.com>
2023-02-16 18:09:48 -0800

    0x00.C-Hello,World projects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-  /* Write a C program that prints exactly "Programming is
+  /**
+   *main - a C program that prints exactly "Programming is
    * like building a multilingual puzzle, followed by a new line.
    */
 
 int main(void)
 
-{ 
-  // Use the function puts
- 
+{
+
 	puts("\"Programming is like building a multilingual puzzle");
-
-  // End program
-
-	return 0;
-}	
+	return (0);
+}
