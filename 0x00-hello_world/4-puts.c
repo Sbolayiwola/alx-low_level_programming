5f363b9a7391903f181eaf98380769ab5625019e
Buraimoh Solomon <Sbolayiwola@gmail.com>
2023-02-16 23:53:47 -0800

    print line with put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-  /**
-   *main - a C program that prints exactly "Programming is
-   * like building a multilingual puzzle, followed by a new line.
-   */
-
+/**
+ * main -Print using puts
+ *
+ * Return: 0
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
