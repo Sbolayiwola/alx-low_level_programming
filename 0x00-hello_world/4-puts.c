8f02d7bd725abb0860939ce22df9f2fe8b77b501
Buraimoh Solomon <Sbolayiwola@gmail.com>
2023-02-16 17:23:50 -0800

    0x00.C-Hello,World projects

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  /* Write a C program that prints exactly "Programming is
+   * like building a multilingual puzzle, followed by a new line.
+   */
+
+int main(void)
+
+{ 
+  // Use the function puts
+ 
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+  // End program
+
+	return (0);
+}	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Write a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ */
+
+int main(void)
+
+{ 
+   //Use the function printf
+
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+
+  // End program
+
+	return (0);
+}		
